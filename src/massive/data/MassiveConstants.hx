83f1eceedbb2b98ec7553ed4eaf9af72c8e391af
Mathieu Sénidre <matse@skwatt.com>
2023-02-13 17:34:26 +0100

    added FLOAT_MAX & FLOAT_MIN constants (to be replaced with something better)

--- src/massive/data/MassiveConstants.hx
+++ src/massive/data/MassiveConstants.hx
@@ -8,6 +8,8 @@ class MassiveConstants
 {
 	public static inline var ANGLE_CONSTANT:Float = 325.94932345220164765467394738691;
 	public static inline var ANGLE_CONSTANT_2:Int = 2047;
+	public static inline var FLOAT_MAX:Float = 999999999;
+	public static inline var FLOAT_MIN:Float = -999999999;
 	public static inline var MAX_QUADS:Int = 16383;
 	public static inline var VERTICES_PER_QUAD:Int = 4;
 }
