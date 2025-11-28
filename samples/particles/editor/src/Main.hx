35fb053b7818e97000f063a674faed18182068a9
Mathieu Sénidre <matse@skwatt.com>
2025-11-28 19:56:14 +0100

    created particle editor (WIP)

---
+++ samples/particles/editor/src/Main.hx
@@ -0,0 +1,51 @@
+package;
+
+import openfl.display.Sprite;
+import openfl.display.StageScaleMode;
+import openfl.display3D.Context3DRenderMode;
+import starling.core.Starling;
+import starling.events.Event;
+
+/**
+ * ...
+ * @author Matse
+ */
+class Main extends Sprite 
+{
+	private var _starling:Starling;
+	
+	public function new() 
+	{
+		super();
+		
+		if (stage != null) start();
+		else addEventListener(openfl.events.Event.ADDED_TO_STAGE, onAddedToStage);
+	}
+	
+	private function onAddedToStage(event:Dynamic):Void
+	{
+		removeEventListener(openfl.events.Event.ADDED_TO_STAGE, onAddedToStage);
+		
+		stage.scaleMode = StageScaleMode.NO_SCALE;
+		
+		start();
+	}
+	
+	private function start():Void
+	{
+		_starling = new Starling(ParticleEditor, stage, null, null, Context3DRenderMode.AUTO, "auto");
+		//_starling.enableErrorChecking = Capabilities.isDebugger;
+		_starling.showStats = true;//Capabilities.isDebugger;
+		_starling.skipUnchangedFrames = true;
+		_starling.supportBrowserZoom = true;
+		_starling.supportHighResolutions = false;
+		_starling.simulateMultitouch = true;
+		_starling.addEventListener(Event.ROOT_CREATED, function():Void
+		{
+			trace("root created");
+		});
+		
+		_starling.start();
+	}
+
+}
---
+++ samples/particles/editor/src/ParticleEditor.hx
@@ -0,0 +1,170 @@
+package;
+
+import feathers.data.ArrayCollection;
+import massive.data.Frame;
+import massive.display.MassiveDisplay;
+import massive.particle.Particle;
+import massive.particle.ParticleSystem;
+import starling.core.Starling;
+import starling.textures.Texture;
+import starling.utils.Align;
+import valedit.ExposedCollection;
+import valeditor.ValEditor;
+import valeditor.data.Data;
+import valeditor.editor.base.ValEditorSimpleStarling;
+import valeditor.editor.file.FileController;
+import valeditor.ui.feathers.data.MenuItem;
+
+/**
+ * ...
+ * @author Matse
+ */
+class ParticleEditor extends ValEditorSimpleStarling 
+{
+	private var _massive:MassiveDisplay;
+	private var _ps:ParticleSystem<Particle>;
+	private var _psCollection:ExposedCollection;
+	
+	// file menu
+	private var _fileMenuCollection:ArrayCollection<MenuItem>;
+	private var _fileOpenItem:MenuItem;
+	private var _fileSaveItem:MenuItem;
+	private var _fileSaveAsItem:MenuItem;
+	
+	// edit menu
+	private var _editMenuCollection:ArrayCollection<MenuItem>;
+	private var _undoItem:MenuItem;
+	private var _redoItem:MenuItem;
+
+	public function new() 
+	{
+		super();
+		
+	}
+	
+	override function exposeData():Void 
+	{
+		Data.exposeMassive();
+		Data.exposeOpenFL_geom();
+		Data.exposeStarling();
+	}
+	
+	override function ready():Void 
+	{
+		super.ready();
+		
+		// file menu
+		this._fileOpenItem = new MenuItem("file_open", "Open", true, "Ctrl+O");
+		this._fileSaveItem = new MenuItem("file_save", "Save", true, "Ctrl+S");
+		this._fileSaveAsItem = new MenuItem("file_save_as", "Save as", true, "Ctrl+Shift+S");
+		this._fileMenuCollection = new ArrayCollection<MenuItem>([
+			this._fileOpenItem,
+			this._fileSaveItem,
+			this._fileSaveAsItem
+		]);
+		this.editView.addMenu("file", "File", onFileMenuCallback, onFileMenuOpen, this._fileMenuCollection);
+		
+		// edit menu
+		this._undoItem = new MenuItem("undo", "Undo", false, "Ctrl+Z");
+		this._redoItem = new MenuItem("redo", "Redo", false, "Ctrl+Y");
+		this._editMenuCollection = new ArrayCollection<MenuItem>([
+			this._undoItem,
+			this._redoItem
+		]);
+		this.editView.addMenu("edit", "Edit", onEditMenuCallback, onEditMenuOpen, this._editMenuCollection);
+		
+		var texture:Texture;
+		var frame:Frame;
+		var frames:Array<Frame>;
+		
+		texture = Texture.fromColor(2, 2);
+		frame = Frame.fromTextureWithAlign(texture, Align.CENTER, Align.CENTER);
+		frames = new Array<Frame>();
+		frames[0] = frame;
+		
+		this._massive = new MassiveDisplay();
+		this._massive.texture = texture;
+		addChild(this._massive);
+		
+		this._ps = new ParticleSystem<Particle>();
+		this._ps.particlesFromPoolFunction = Particle.fromPoolArray;
+		this._ps.particlesToPoolFunction = Particle.toPoolArray;
+		this._ps.addFrames(frames);
+		this._massive.addLayer(this._ps);
+		
+		this._ps.start();
+		
+		this._psCollection = ValEditor.edit(this._ps);
+	}
+	
+	@:access(starling.core.Starling)
+	override function onDisplayResize(evt:openfl.events.Event):Void 
+	{
+		super.onDisplayResize(evt);
+		
+		if (Starling.current.showStats)
+		{
+			Starling.current.__statsDisplay.x = this.editView.displayRect.x;
+			Starling.current.__statsDisplay.y = this.editView.displayRect.y;
+		}
+		
+		if (this._ps == null) return;
+		this._ps.emitterX = this.editView.displayCenter.x;
+		this._ps.emitterY = this.editView.displayCenter.y;
+		this._psCollection.read();
+	}
+	
+	@:access(starling.core.Starling)
+	override function updateViewPort(width:Int, height:Int):Void 
+	{
+		super.updateViewPort(width, height);
+		
+		if (Starling.current.showStats)
+		{
+			Starling.current.__statsDisplay.x = this.editView.displayRect.x;
+			Starling.current.__statsDisplay.y = this.editView.displayRect.y;
+		}
+	}
+	
+	private function onEditMenuOpen(evt:openfl.events.Event):Void
+	{
+		this._undoItem.enabled = ValEditor.actionStack.canUndo;
+		this._redoItem.enabled = ValEditor.actionStack.canRedo;
+		this._editMenuCollection.updateAll();
+	}
+	
+	private function onEditMenuCallback(item:MenuItem):Void
+	{
+		switch (item.id)
+		{
+			case "undo" :
+				ValEditor.actionStack.undo();
+			
+			case "redo" :
+				ValEditor.actionStack.redo();
+		}
+	}
+	
+	private function onFileMenuOpen(evt:openfl.events.Event):Void
+	{
+		
+	}
+	
+	private function onFileMenuCallback(item:MenuItem):Void
+	{
+		switch (item.id)
+		{
+			case "file_open" :
+				//FileController.open(
+			
+			case "file_save" :
+				
+			
+			case "file_save_as" :
+				
+		}
+	}
+	
+	//private function onFileOpen(
+	
+}
