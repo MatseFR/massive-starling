ded0efd39af02891371bf6a56b13fe2ec003bb40
Mathieu Sénidre <matse@skwatt.com>
2026-01-13 18:12:11 +0100

    ParticleSystemDefaults +create() function

--- src/massive/particle/ParticleSystemDefaults.hx
+++ src/massive/particle/ParticleSystemDefaults.hx
@@ -10,4 +10,17 @@ class ParticleSystemDefaults
 	static public var AUTO_CLEAR_ON_COMPLETE:Bool = true;
 	static public var RANDOM_SEED:Int = 1;
 	
+	static public function create(?options:ParticleSystemOptions):ParticleSystem<Particle>
+	{
+		var ps:ParticleSystem<Particle> = new ParticleSystem(options);
+		#if flash
+		ps.particlesFromPoolFunction = Particle.fromPoolVector;
+		ps.particlesToPoolFunction = Particle.toPoolVector;
+		#else
+		ps.particlesFromPoolFunction = Particle.fromPoolArray;
+		ps.particlesToPoolFunction = Particle.toPoolArray;
+		#end
+		return ps;
+	}
+	
 }
