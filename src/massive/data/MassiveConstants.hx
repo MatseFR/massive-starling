b5a4e9a3ecaedb91a1f6a03cd7871519534f271f
Mathieu Sénidre <matse@skwatt.com>
2026-01-26 16:14:26 +0100

    MassiveConstants +INDICES_PER_QUAD

--- src/massive/data/MassiveConstants.hx
+++ src/massive/data/MassiveConstants.hx
@@ -8,6 +8,7 @@ class MassiveConstants
 {
 	static public inline var ANGLE_CONSTANT:Float = 325.94932345220164765467394738691;
 	static public inline var ANGLE_CONSTANT_2:Int = 2047;
+	static public inline var INDICES_PER_QUAD:Int = 6;
 	static public inline var MAX_QUADS:Int = 16383;
 	static public inline var VERTICES_PER_QUAD:Int = 4;
 }
