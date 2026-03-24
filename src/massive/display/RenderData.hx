5fa0c944e22937f62fe02ebc8042b38418b545a1
Mathieu Sénidre <matse@skwatt.com>
2026-03-24 18:50:24 +0100

    RenderData +pma +useAlpha +useColor +useColorOffset +useDisplayColor +useSimpleColor

--- src/massive/display/RenderData.hx
+++ src/massive/display/RenderData.hx
@@ -8,9 +8,15 @@ class RenderData
 {
 	public var multiTexturing:Bool;
 	public var numQuads:Int;
+	public var pma:Bool = true;
 	public var position:Int;
 	public var quadOffset:Int;
 	public var totalQuads:Int;
+	public var useAlpha:Bool;
+	public var useColor:Bool;
+	public var useColorOffset:Bool;
+	public var useDisplayColor:Bool;
+	public var useSimpleColor:Bool;
 	
 	public function new() 
 	{
