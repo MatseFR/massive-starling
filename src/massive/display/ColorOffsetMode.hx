253ea2da24e06df0a1c5410ba58e640e3eeae370
Mathieu Sénidre <matse@skwatt.com>
2026-03-24 18:50:46 +0100

    created ColorOffsetMode

---
+++ src/massive/display/ColorOffsetMode.hx
@@ -0,0 +1,28 @@
+package massive.display;
+
+/**
+ * ...
+ * @author Matse
+ */
+class ColorOffsetMode 
+{
+	/**
+	   
+	**/
+	static public inline var DISPLAY:String = "display";
+	
+	/**
+	   
+	**/
+	static public inline var DISPLAY_AND_OBJECT:String = "display_and_object";
+	
+	/**
+	   
+	**/
+	static public inline var NONE:String = "none";
+	
+	/**
+	   
+	**/
+	static public inline var OBJECT:String = "object";
+}
