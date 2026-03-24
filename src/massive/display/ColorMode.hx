f1462f0755b7b77f5c32a3d9fdae19b3f7ff5f53
Mathieu Sénidre <matse@skwatt.com>
2026-03-24 18:18:28 +0100

    MassiveColorMode renamed to ColorMode, +DISPLAY mode (+ALPHA mode but not used yet)

--- src/massive/display/ColorMode.hx
+++ src/massive/display/ColorMode.hx
@@ -4,15 +4,25 @@ package massive.display;
  * ...
  * @author Matse
  */
-class MassiveColorMode 
+class ColorMode 
 {
 	/**
+	   
+	**/
+	static public inline var ALPHA:String = "alpha";
+	/**
+	   Objects color and alpha is only affected by MassiveDisplay color and alpha
+	**/
+	static public inline var DISPLAY:String = "display";
+	/**
+	   Objects color and alpha are affected by both their own color/alpha and the MassiveDisplay color/alpha
 	   Handles color values over 1.0, each color channel takes 32 bits.
 	   This is the default value.
 	**/
 	static public inline var EXTENDED:String = "extended";
 	/**
 	   No color or alpha, textures still work.
+	   CAUTION : MassiveDisplay alpha and parent containers alpha won't have any effect
 	**/
 	static public inline var NONE:String = "none";
 	/**
@@ -23,7 +33,7 @@ class MassiveColorMode
 	
 	static public function getValues():Array<String>
 	{
-		return [EXTENDED, NONE, REGULAR];
+		return [NONE, DISPLAY, REGULAR, EXTENDED];
 	}
 	
 }
