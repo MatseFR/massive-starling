d9cf48cd19b331ce0ab05e099cd2947b7ab9314a
Mathieu Sénidre <matse@skwatt.com>
2026-04-21 23:39:15 +0200

    EmitterType +helper functions for ValEditor

--- src/massive/particle/EmitterType.hx
+++ src/massive/particle/EmitterType.hx
@@ -8,4 +8,14 @@ class EmitterType
 {
 	static public inline var GRAVITY:Int = 0;
 	static public inline var RADIAL:Int = 1;
+	
+	static public function getNames():Array<String>
+	{
+		return ["Gravity", "Radial"];
+	}
+	
+	static public function getValues():Array<Int>
+	{
+		return [GRAVITY, RADIAL];
+	}
 }
