aacacfe9b1c613de9cb810bb0bd2988f9b22cafc
Mathieu Sénidre <matse@skwatt.com>
2026-04-28 15:42:00 +0200

    OscillationFrequencyStart : commented out values not in use yet

--- src/massive/particle/OscillationFrequencyStart.hx
+++ src/massive/particle/OscillationFrequencyStart.hx
@@ -6,11 +6,14 @@ package massive.particle;
 class OscillationFrequencyStart
 {
 	inline public static var ZERO:String = "zero";
+	//inline public static var INCREMENTAL:String = "incremental";
+	//inline public static var UNIFIED_INCREMENTAL:String = "unified_incremental";
 	inline public static var RANDOM:String = "random";
 	inline public static var UNIFIED_RANDOM:String = "unified_random";
 	
 	public static function getValues():Array<String>
 	{
+		//return [ZERO, INCREMENTAL, UNIFIED_INCREMENTAL, RANDOM, UNIFIED_RANDOM];
 		return [ZERO, RANDOM, UNIFIED_RANDOM];
 	}
 }
