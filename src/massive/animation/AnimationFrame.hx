1e0e61d2878db474bdeac8b50a2be9f31a395b7f
Mathieu Sénidre <matse@skwatt.com>
2026-06-12 22:31:38 +0200

    AnimationFrame : optionnal VertexData/VertexColorData/VertexColorExData

--- src/massive/animation/AnimationFrame.hx
+++ src/massive/animation/AnimationFrame.hx
@@ -1,5 +1,8 @@
 package massive.animation;
 import massive.data.Frame;
+import massive.data.VertexColorData;
+import massive.data.VertexColorExData;
+import massive.data.VertexData;
 
 /**
  * ...
@@ -9,7 +12,8 @@ class AnimationFrame
 {
 	static private var _POOL:Array<AnimationFrame> = new Array<AnimationFrame>();
 	
-	static public function fromPool(frame:Frame = null, timing:Float = 0.0, event:String = null, eventParams:Dynamic = null):AnimationFrame
+	static public function fromPool(frame:Frame = null, timing:Float = 0.0, event:String = null, eventParams:Dynamic = null, 
+									vertexData:VertexData = null, vertexColorData:VertexColorData = null, vertexColorExData:VertexColorExData = null):AnimationFrame
 	{
 		if (_POOL.length != 0) return _POOL.pop().setFromPool(frame, timing, event, eventParams);
 		return new AnimationFrame(frame, timing, event, eventParams);
@@ -19,13 +23,20 @@ class AnimationFrame
 	public var eventParams:Dynamic;
 	public var frame:Frame;
 	public var timing:Float;
+	public var vertexData:VertexData;
+	public var vertexColorData:VertexColorData;
+	public var vertexColorExData:VertexColorExData;
 
-	public function new(frame:Frame = null, timing:Float = 0.0, event:String = null, eventParams:Dynamic = null) 
+	public function new(frame:Frame = null, timing:Float = 0.0, event:String = null, eventParams:Dynamic = null, 
+						vertexData:VertexData = null, vertexColorData:VertexColorData = null, vertexColorExData:VertexColorExData = null) 
 	{
 		this.frame = frame;
 		this.timing = timing;
 		this.event = event;
 		this.eventParams = eventParams;
+		this.vertexData = vertexData;
+		this.vertexColorData = vertexColorData;
+		this.vertexColorExData = vertexColorExData;
 	}
 	
 	public function clear():Void
@@ -34,6 +45,9 @@ class AnimationFrame
 		this.eventParams = null;
 		this.frame = null;
 		this.timing = 0.0;
+		this.vertexData = null;
+		this.vertexColorData = null;
+		this.vertexColorExData = null;
 	}
 	
 	public function pool():Void
@@ -42,12 +56,16 @@ class AnimationFrame
 		_POOL[_POOL.length] = this;
 	}
 	
-	private function setFromPool(frame:Frame, timing:Float = 0.0, event:String = null, eventParams:Dynamic = null):AnimationFrame
+	private function setFromPool(frame:Frame, timing:Float = 0.0, event:String = null, eventParams:Dynamic = null, 
+								 vertexData:VertexData = null, vertexColorData:VertexColorData = null, vertexColorExData:VertexColorExData = null):AnimationFrame
 	{
 		this.frame = frame;
 		this.timing = timing;
 		this.event = event;
 		this.eventParams = eventParams;
+		this.vertexData = vertexData;
+		this.vertexColorData = vertexColorData;
+		this.vertexColorExData = vertexColorExData;
 		return this;
 	}
 	
