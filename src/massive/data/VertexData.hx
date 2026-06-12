898663437afadaab7ec5bad7b10f816dd48a63d0
Mathieu Sénidre <matse@skwatt.com>
2026-06-12 22:18:59 +0200

    created VertexData

---
+++ src/massive/data/VertexData.hx
@@ -0,0 +1,62 @@
+package massive.data;
+
+/**
+ * ...
+ * @author Matse
+ */
+class VertexData 
+{
+	static private var _POOL:Array<VertexData> = new Array<VertexData>();
+	
+	static public function fromPool(x1:Float = 0.0, x2:Float = 0.0, x3:Float = 0.0, x4:Float = 0.0, y1:Float = 0.0, y2:Float = 0.0, y3:Float = 0.0, y4:Float = 0.0):VertexData
+	{
+		if (_POOL.length != 0) return _POOL.pop().setFromPool(x1, x2, x3, x4, y1, y2, y3, y4);
+		return new VertexData(x1, x2, x3, x4, y1, y2, y3, y4);
+	}
+	
+	public var x1:Float;
+	public var x2:Float;
+	public var x3:Float;
+	public var x4:Float;
+	public var y1:Float;
+	public var y2:Float;
+	public var y3:Float;
+	public var y4:Float;
+
+	public function new(x1:Float = 0.0, x2:Float = 0.0, x3:Float = 0.0, x4:Float = 0.0, y1:Float = 0.0, y2:Float = 0.0, y3:Float = 0.0, y4:Float = 0.0) 
+	{
+		this.x1 = x1;
+		this.x2 = x2;
+		this.x3 = x3;
+		this.x4 = x4;
+		this.y1 = y1;
+		this.y2 = y2;
+		this.y3 = y3;
+		this.y4 = y4;
+	}
+	
+	public function clear():Void
+	{
+		this.x1 = this.x2 = this.x3 = this.x4 = this.y1 = this.y2 = this.y3 = this.y4 = 0.0;
+	}
+	
+	public function pool():Void
+	{
+		// no need to call clear()
+		_POOL[_POOL.length] = this;
+	}
+	
+	private function setFromPool(x1:Float = 0.0, x2:Float = 0.0, x3:Float = 0.0, x4:Float = 0.0, y1:Float = 0.0, y2:Float = 0.0, y3:Float = 0.0, y4:Float = 0.0):VertexData
+	{
+		this.x1 = x1;
+		this.x2 = x2;
+		this.x3 = x3;
+		this.x4 = x4;
+		this.y1 = y1;
+		this.y2 = y2;
+		this.y3 = y3;
+		this.y4 = y4;
+		return this;
+	}
+	
+}
