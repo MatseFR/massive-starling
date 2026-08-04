ce481728bd494b5e311cfd29cb6579868ca758ec
Mathieu Sénidre <matse@skwatt.com>
2026-08-04 17:19:13 +0200

    Animator : reflect changes + completed animation code

--- src/massive/animation/Animator.hx
+++ src/massive/animation/Animator.hx
@@ -1,8 +1,8 @@
 package massive.animation;
 
-import massive.display.BasicClip;
 import massive.display.Clip;
-import massive.event.MassiveEvent;
+import massive.display.EventClip;
+import massive.event.MassiveEventType;
 import massive.particle.Particle;
 import starling.animation.IAnimatable;
 #if flash
@@ -22,32 +22,32 @@ class Animator implements IAnimatable
 	public var animate:Bool = true;
 	
 	#if flash
-	private var _basicClipLists:Vector<Vector<BasicClip>>;
-	private var _basicClips:Vector<BasicClip>;
-	private var _clipLists:Vector<Vector<Clip>>;
-	private var _clips:Vector<Clip>;
+	private var _basicClipLists:Vector<Vector<Clip>>;
+	private var _basicClips:Vector<Clip>;
+	private var _clipLists:Vector<Vector<EventClip>>;
+	private var _clips:Vector<EventClip>;
 	private var _particleLists:Vector<Vector<Particle>>;
 	#else
-	private var _basicClipLists:Array<Array<BasicClip>>;
-	private var _basicClips:Array<BasicClip>;
-	private var _clipLists:Array<Array<Clip>>;
-	private var _clips:Array<Clip>;
+	private var _basicClipLists:Array<Array<Clip>>;
+	private var _basicClips:Array<Clip>;
+	private var _clipLists:Array<Array<EventClip>>;
+	private var _clips:Array<EventClip>;
 	private var _particleLists:Array<Array<Particle>>;
 	#end
 	
 	public function new()
 	{
 		#if flash
-		this._basicClipLists = new Vector<Vector<BasicClip>>();
-		this._basicClips = new Vector<BasicClip>();
-		this._clipLists = new Vector<Vector<Clip>>();
-		this._clips = new Vector<Clip>();
+		this._basicClipLists = new Vector<Vector<Clip>>();
+		this._basicClips = new Vector<Clip>();
+		this._clipLists = new Vector<Vector<EventClip>>();
+		this._clips = new Vector<EventClip>();
 		this._particleLists = new Vector<Vector<Particle>>();
 		#else
-		this._basicClipLists = new Array<Array<BasicClip>>();
-		this._basicClips = new Array<BasicClip>();
-		this._clipLists = new Array<Array<Clip>>();
-		this._clips = new Array<Clip>();
+		this._basicClipLists = new Array<Array<Clip>>();
+		this._basicClips = new Array<Clip>();
+		this._clipLists = new Array<Array<EventClip>>();
+		this._clips = new Array<EventClip>();
 		this._particleLists = new Array<Array<Particle>>();
 		#end
 	}
@@ -69,17 +69,17 @@ class Animator implements IAnimatable
 		#end
 	}
 	
-	public function addBasicClip(clip:BasicClip):Void
+	public function addBasicClip(clip:Clip):Void
 	{
 		this._basicClips[this._basicClips.length] = clip;
 	}
 	
-	public function hasBasicClip(clip:BasicClip):Bool
+	public function hasBasicClip(clip:Clip):Bool
 	{
 		return this._basicClips.indexOf(clip) != -1;
 	}
 	
-	public function removeBasicClip(clip:BasicClip):Void
+	public function removeBasicClip(clip:Clip):Void
 	{
 		#if flash
 		this._basicClips.removeAt(this._basicClips.indexOf(clip));
@@ -89,27 +89,27 @@ class Animator implements IAnimatable
 	}
 	
 	#if flash
-	public function addBasicClipList(clips:Vector<BasicClip>):Void
+	public function addBasicClipList(clips:Vector<Clip>):Void
 	#else
-	public function addBasicClipList(clips:Array<BasicClip>):Void
+	public function addBasicClipList(clips:Array<Clip>):Void
 	#end
 	{
 		this._basicClipLists[this._basicClipLists.length] = clips;
 	}
 	
 	#if flash
-	public function hasBasicClipList(clips:Vector<BasicClip>):Bool
+	public function hasBasicClipList(clips:Vector<Clip>):Bool
 	#else
-	public function hasBasicClipList(clips:Array<BasicClip>):Bool
+	public function hasBasicClipList(clips:Array<Clip>):Bool
 	#end
 	{
 		return this._basicClipLists.indexOf(clips) != -1;
 	}
 	
 	#if flash
-	public function removeBasicClipList(clips:Vector<BasicClip>):Void
+	public function removeBasicClipList(clips:Vector<Clip>):Void
 	#else
-	public function removeBasicClipList(clips:Array<BasicClip>):Void
+	public function removeBasicClipList(clips:Array<Clip>):Void
 	#end
 	{
 		#if flash
@@ -119,17 +119,17 @@ class Animator implements IAnimatable
 		#end
 	}
 	
-	public function addClip(clip:Clip):Void
+	public function addClip(clip:EventClip):Void
 	{
 		this._clips[this._clips.length] = clip;
 	}
 	
-	public function hasClip(clip:Clip):Bool
+	public function hasClip(clip:EventClip):Bool
 	{
 		return this._clips.indexOf(clip) != -1;
 	}
 	
-	public function removeClip(clip:Clip):Void
+	public function removeClip(clip:EventClip):Void
 	{
 		#if flash
 		this._clips.removeAt(this._clips.indexOf(clip));
@@ -139,27 +139,27 @@ class Animator implements IAnimatable
 	}
 	
 	#if flash
-	public function addClipList(clips:Vector<Clip>):Void
+	public function addClipList(clips:Vector<EventClip>):Void
 	#else
-	public function addClipList(clips:Array<Clip>):Void
+	public function addClipList(clips:Array<EventClip>):Void
 	#end
 	{
 		this._clipLists[this._clipLists.length] = clips;
 	}
 	
 	#if flash
-	public function hasClipList(clips:Vector<Clip>):Bool
+	public function hasClipList(clips:Vector<EventClip>):Bool
 	#else
-	public function hasClipList(clips:Array<Clip>):Bool
+	public function hasClipList(clips:Array<EventClip>):Bool
 	#end
 	{
 		return this._clipLists.indexOf(clips) != -1;
 	}
 	
 	#if flash
-	public function removeClipList(clips:Vector<Clip>):Void
+	public function removeClipList(clips:Vector<EventClip>):Void
 	#else
-	public function removeClipList(clips:Array<Clip>):Void
+	public function removeClipList(clips:Array<EventClip>):Void
 	#end
 	{
 		#if flash
@@ -206,18 +206,18 @@ class Animator implements IAnimatable
 		
 		var count:Int;
 		
-		if (this._basicClips.length != 0) animateBasicClips(this._basicClips, time);
+		if (this._basicClips.length != 0) animateClips(this._basicClips, time);
 		count = this._basicClipLists.length;
 		for (i in 0...count)
 		{
-			animateBasicClips(this._basicClipLists[i], time);
+			animateClips(this._basicClipLists[i], time);
 		}
 		
-		if (this._clips.length != 0) animateClips(this._clips, time);
+		if (this._clips.length != 0) animateEventClips(this._clips, time);
 		count = this._clipLists.length;
 		for (i in 0...count)
 		{
-			animateClips(this._clipLists[i], time);
+			animateEventClips(this._clipLists[i], time);
 		}
 		
 		//count = this._particleLists.length;
@@ -227,14 +227,14 @@ class Animator implements IAnimatable
 		//}
 	}
 	
-	@:access(massive.display.BasicClip)
+	@:access(massive.display.Clip)
 	#if flash
-	inline private function animateBasicClips(clips:Vector<BasicClip>, time:Float):Void
+	inline private function animateClips(clips:Vector<Clip>, time:Float):Void
 	#else
-	inline private function animateBasicClips(clips:Array<BasicClip>, time:Float):Void
+	inline private function animateClips(clips:Array<Clip>, time:Float):Void
 	#end
 	{
-		var clip:BasicClip;
+		var clip:Clip;
 		var count:Int = clips.length;
 		for (i in 0...count)
 		{
@@ -254,58 +254,37 @@ class Animator implements IAnimatable
 					clip.frameTime -= clip.animation.loopDuration;
 					++clip.loopCount;
 				}
-				else if (clip.completeCallback != null)
+				else
 				{
-					clip.completeCallback(clip);
+					clip.animationComplete = true;
+					if (clip.animationCompleteCallback != null) clip.animationCompleteCallback(clip);
+					if (clip.animation.nextAnimationID != null)
+					{
+						clip.playWithID(clip.animation.nextAnimationID);
+					}
+					else if (clip._animationQueue.length != 0)
+					{
+						clip.playNextFromQueue();
+					}
+					else if (clip.completeCallback != null)
+					{
+						clip.completeCallback(clip);
+					}
 				}
 			}
 		}
 	}
 	
-	@:access(massive.display.Clip)
+	@:access(massive.display.EventClip)
 	#if flash
-	inline private function animateClips(clips:Vector<Clip>, time:Float):Void
+	inline private function animateEventClips(clips:Vector<EventClip>, time:Float):Void
 	#else
-	inline private function animateClips(clips:Array<Clip>, time:Float):Void
+	inline private function animateEventClips(clips:Array<EventClip>, time:Float):Void
 	#end
 	{
-		var clip:Clip;
+		var clip:EventClip;
 		var frameIndex:Int;
 		var count:Int = clips.length;
-		//for (i in 0...count)
-		//{
-			//clip = clips[i];
-			//if (!clip.animate) continue;
-			//
-			//clip.frameTime += time * clip.frameDelta;
-			//if (clip.frameTime >= clip.frameTimingCurrent)
-			//{
-				//frameIndex = clip._frameIndex;
-				//while (true)
-				//{
-					//if (frameIndex < clip.lastFrameIndex)
-					//{
-						//++frameIndex;
-						//clip.frameTimingCurrent = clip._animationFrames[frameIndex].timing;
-					//}
-					//else if (clip.loop && (clip.numLoops == 0 || clip.loopCount < clip.numLoops))
-					//{
-						//frameIndex = clip.animation.loopFrame;
-						//++clip.loopCount;
-						//clip.frameTime -= clip.animation.loopDuration;
-						//clip.frameTimingCurrent = clip._animationFrames[frameIndex].timing;
-					//}
-					//else
-					//{
-						//// animation complete
-						//break;
-					//}
-					//if (clip.frameTime < clip.frameTimingCurrent) break;
-				//}
-				//clip.frameIndex = frameIndex;
-			//}
-		//}
-		
 		for (i in 0...count)
 		{
 			clip = clips[i];
@@ -336,7 +315,25 @@ class Animator implements IAnimatable
 					else
 					{
 						// animation complete
-						clip.dispatchEventWith(MassiveEvent.ANIMATION_COMPLETE);
+						clip.animationComplete = true;
+						clip.dispatchEventWith(MassiveEventType.ANIMATION_COMPLETE);
+						if (clip.animationCompleteCallback != null) clip.animationCompleteCallback(clip);
+						if (clip.animation.nextAnimationID != null)
+						{
+							clip.playWithID(clip.animation.nextAnimationID);
+						}
+						else if (clip._animationQueue.length != 0)
+						{
+							clip.playNextFromQueue();
+						}
+						else
+						{
+							clip.dispatchEventWith(MassiveEventType.CLIP_COMPLETE);
+							if (clip.completeCallback != null)
+							{
+								clip.completeCallback(clip);
+							}
+						}
 						break;
 					}
 					if (clip.frameTime < clip.frameTimingCurrent) break;
