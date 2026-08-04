b4f2a325c9c5d6083f5a86f8c12cf44c21aa9180
Mathieu Sénidre <matse@skwatt.com>
2026-08-04 17:20:02 +0200

    completed QueuedAnimation

--- src/massive/animation/QueuedAnimation.hx
+++ src/massive/animation/QueuedAnimation.hx
@@ -1,4 +1,5 @@
 package massive.animation;
+import massive.display.Clip;
 
 /**
  * ...
@@ -8,37 +9,46 @@ class QueuedAnimation
 {
 	static private var _POOL:Array<QueuedAnimation> = new Array<QueuedAnimation>();
 	
-	static public function fromPool(animation:Animation, frameIndex:Int = 0):QueuedAnimation
+	static public function fromPool(animation:Animation, frameIndex:Int = 0, numLoops:Int = -1, animationCompleteCallback:Clip->Void = null, removeOnPlay:Bool = false):QueuedAnimation
 	{
-		if (_POOL.length != 0) return _POOL.pop();
-		return new QueuedAnimation(animation, frameIndex);
+		if (_POOL.length != 0) return _POOL.pop().setFromPool(animation, frameIndex, numLoops, animationCompleteCallback, removeOnPlay);
+		return new QueuedAnimation(animation, frameIndex, numLoops, animationCompleteCallback, removeOnPlay);
 	}
 	
 	public var animation:Animation;
+	public var animationCompleteCallback:Clip->Void;
 	public var frameIndex:Int;
+	public var numLoops:Int;
+	public var removeOnPlay:Bool;
 
-	public function new(animation:Animation, frameIndex:Int = 0) 
+	/**
+	   
+	   @param	animation
+	   @param	frameIndex
+	   @param	numLoops	how many loops : -1 = use animation setting, 0 = infinite
+	   @param	removeOnPlay
+	**/
+	public function new(animation:Animation, frameIndex:Int = 0, numLoops:Int = -1, animationCompleteCallback:Clip->Void = null, removeOnPlay:Bool = false) 
 	{
 		this.animation = animation;
 		this.frameIndex = frameIndex;
-	}
-	
-	public function clear():Void
-	{
-		this.animation = null;
-		this.frameIndex = 0;
+		this.numLoops = numLoops;
+		this.animationCompleteCallback = animationCompleteCallback;
+		this.removeOnPlay = removeOnPlay;
 	}
 	
 	public function pool():Void
 	{
-		clear();
 		_POOL[_POOL.length] = this;
 	}
 	
-	private function setFromPool(animation:Animation, frameIndex:Int):QueuedAnimation
+	private function setFromPool(animation:Animation, frameIndex:Int, numLoops:Int, animationCompleteCallback:Clip->Void, removeOnPlay:Bool):QueuedAnimation
 	{
 		this.animation = animation;
 		this.frameIndex = frameIndex;
+		this.numLoops = numLoops;
+		this.animationCompleteCallback = animationCompleteCallback;
+		this.removeOnPlay = removeOnPlay;
 		return this;
 	}
 	
