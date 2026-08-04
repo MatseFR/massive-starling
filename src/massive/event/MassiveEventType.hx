8bf3b06c12212e93a070876ade71aede67e81737
Mathieu Sénidre <matse@skwatt.com>
2026-08-04 17:18:46 +0200

    MassiveEvent now MassiveEventType, +CLIP_COMPLETE

--- src/massive/event/MassiveEventType.hx
+++ src/massive/event/MassiveEventType.hx
@@ -4,9 +4,10 @@ package massive.event;
  * ...
  * @author Matse
  */
-class MassiveEvent 
+class MassiveEventType 
 {
 
 	inline static public var ANIMATION_COMPLETE:String = "animation_complete";
+	inline static public var CLIP_COMPLETE:String = "clip_complete";
 	
 }
