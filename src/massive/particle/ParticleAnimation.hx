46c37c211df72cc9602f2c88b0d708a3e6bf041e
Mathieu Sénidre <matse@skwatt.com>
2026-08-27 16:24:09 +0200

    ParticleAnimation +random start frame & random loops

--- src/massive/particle/ParticleAnimation.hx
+++ src/massive/particle/ParticleAnimation.hx
@@ -9,21 +9,38 @@ class ParticleAnimation
 {
 	static private var _POOL:Array<ParticleAnimation> = new Array<ParticleAnimation>();
 	
-	static public function fromPool(animation:Animation, weight:Float = 1.0, textureIndex:Int = 0):ParticleAnimation
+	static public function fromPool(animation:Animation, weight:Float = 1.0, textureIndex:Int = 0, randomStartFrameMin:Int = -1, randomStartFrameMax:Int = -1, randomLoopMin:Int = -1, randomLoopMax:Int = -1):ParticleAnimation
 	{
-		if (_POOL.length != 0) return _POOL.pop();
-		return new ParticleAnimation(animation, weight, textureIndex);
+		if (_POOL.length != 0) return _POOL.pop().setFromPool(animation, weight, textureIndex, randomStartFrameMin, randomStartFrameMax, randomLoopMin, randomLoopMax);
+		return new ParticleAnimation(animation, weight, textureIndex, randomStartFrameMin, randomStartFrameMax, randomLoopMin, randomLoopMax);
 	}
 	
 	public var animation:Animation;
 	public var textureIndex:Int;
 	public var weight:Float;
+	public var randomLoops:Bool;
+	public var randomLoopMax:Int;
+	public var randomLoopMin:Int;
+	public var randomStart:Bool;
+	public var randomStartFrameMax:Int;
+	public var randomStartFrameMin:Int;
 
-	public function new(animation:Animation, weight:Float = 1.0, textureIndex:Int = 0) 
+	public function new(animation:Animation, weight:Float = 1.0, textureIndex:Int = 0, randomStartFrameMin:Int = -1, randomStartFrameMax:Int = -1, randomLoopMin:Int = -1, randomLoopMax:Int = -1) 
 	{
 		this.animation = animation;
 		this.weight = weight;
 		this.textureIndex = textureIndex;
+		this.randomStart = randomStartFrameMin > -1 || randomStartFrameMax > -1;
+		if (this.randomStart)
+		{
+			if (randomStartFrameMin < 0) randomStartFrameMin = 0;
+			if (randomStartFrameMax < 0) randomStartFrameMax = animation.lastFrameIndex;
+		}
+		this.randomStartFrameMin = randomStartFrameMin;
+		this.randomStartFrameMax = randomStartFrameMax;
+		this.randomLoops = randomLoopMin > -1 && randomLoopMax > -1;
+		this.randomLoopMin = randomLoopMin;
+		this.randomLoopMax = randomLoopMax;
 	}
 	
 	public function clear():Void
@@ -37,11 +54,22 @@ class ParticleAnimation
 		_POOL[_POOL.length] = this;
 	}
 	
-	private function setFromPool(animation:Animation, weight:Float, textureIndex:Int):ParticleAnimation
+	private function setFromPool(animation:Animation, weight:Float, textureIndex:Int, randomStartFrameMin:Int = -1, randomStartFrameMax:Int = -1, randomLoopMin:Int = -1, randomLoopMax:Int = -1):ParticleAnimation
 	{
 		this.animation = animation;
 		this.weight = weight;
 		this.textureIndex = textureIndex;
+		this.randomStart = randomStartFrameMin > -1 || randomStartFrameMax > -1;
+		if (this.randomStart)
+		{
+			if (randomStartFrameMin < 0) randomStartFrameMin = 0;
+			if (randomStartFrameMax < 0) randomStartFrameMax = animation.lastFrameIndex;
+		}
+		this.randomStartFrameMin = randomStartFrameMin;
+		this.randomStartFrameMax = randomStartFrameMax;
+		this.randomLoops = randomLoopMin > -1 && randomLoopMax > -1;
+		this.randomLoopMin = randomLoopMin;
+		this.randomLoopMax = randomLoopMax;
 		return this;
 	}
 	
