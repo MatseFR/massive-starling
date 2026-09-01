e319b273b884f17b0e8678b69ae796ad5b38cbff
Mathieu Sénidre <matse@skwatt.com>
2026-09-01 16:06:27 +0200

    RenderData +color and color offset vars

--- src/massive/display/render/RenderData.hx
+++ src/massive/display/render/RenderData.hx
@@ -19,6 +19,16 @@ class RenderData
 	public var useDisplayColor:Bool;
 	public var useSimpleColor:Bool;
 	
+	public var red:Float = 1.0;
+	public var green:Float = 1.0;
+	public var blue:Float = 1.0;
+	public var alpha:Float = 1.0;
+	
+	public var redOffset:Float = 0.0;
+	public var greenOffset:Float = 0.0;
+	public var blueOffset:Float = 0.0;
+	public var alphaOffset:Float = 0.0;
+	
 	public function new(display:MassiveDisplay) 
 	{
 		this.display = display;
@@ -28,6 +38,8 @@ class RenderData
 	public function clear():Void
 	{
 		this.numQuads = this.position = this.quadOffset = this.totalQuads = 0;
+		this.red = this.green = this.blue = this.alpha = 1.0;
+		this.redOffset = this.greenOffset = this.blueOffset = this.alphaOffset = 0.0;
 	}
 	
 	public function dispose():Void
