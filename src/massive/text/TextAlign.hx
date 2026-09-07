8e4e226c238e8e82cba92e49f3ef653b5fbd672e
Mathieu Sénidre <matse@skwatt.com>
2026-09-07 17:38:29 +0200

    created TextAlign

---
+++ src/massive/text/TextAlign.hx
@@ -0,0 +1,15 @@
+package massive.text;
+
+/**
+ * ...
+ * @author Matse
+ */
+class TextAlign 
+{
+
+	public static inline var CENTER:String = "center";
+	public static inline var JUSTIFY:String = "justify";
+	public static inline var LEFT:String = "left";
+	public static inline var RIGHT:String = "right";
+	
+}
