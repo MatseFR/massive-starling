94e5091de4c510ec2c3d4b7df745e83b73636fce
Mathieu Sénidre <matse@skwatt.com>
2026-09-24 17:01:24 +0200

    TextAlign +BOTTOM +TOP

--- src/massive/text/TextAlign.hx
+++ src/massive/text/TextAlign.hx
@@ -7,9 +7,11 @@ package massive.text;
 class TextAlign 
 {
 
+	public static inline var BOTTOM:String = "bottom";
 	public static inline var CENTER:String = "center";
 	public static inline var JUSTIFY:String = "justify";
 	public static inline var LEFT:String = "left";
 	public static inline var RIGHT:String = "right";
+	public static inline var TOP:String = "top";
 	
 }
